fe6f8fd982e01239ff35dae2f92aeb370e888ea1
Norbert <norbert_2000@o2.pl>
2017-06-07 21:56:28 +0200

    completed program

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <conio.h>
+#include <string>
 
 int main()
 {
-	std::cout << "reverse a string" << std::endl;
+	std::string string;
+	std::string reversed;
+
+	std::cout << "Enter the string: " << std::endl;
+	std::cin >> string;
+
+	for (int i = string.size() - 1; i >= 0; i--) // reverse string
+	{
+		reversed.push_back(string[i]);
+	}
+
+	std::cout << "Reversed string: " << std::endl;
+	std::cout << reversed;
 
 
 	getch();
